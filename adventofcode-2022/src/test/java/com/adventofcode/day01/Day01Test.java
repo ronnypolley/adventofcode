c532e4c9bd357a9a8a40caafb1fe6fc6c61d1c0f
Ronny Polley <ronnypolley@googlemail.com>
2022-12-03 14:27:04 +0100

    refactor code to only use streams

--- adventofcode-2022/src/test/java/com/adventofcode/day01/Day01Test.java
+++ adventofcode-2022/src/test/java/com/adventofcode/day01/Day01Test.java
@@ -9,6 +9,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
@@ -18,37 +19,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 class Day01Test {
 
 	@ParameterizedTest
-	@ValueSource(strings = {"day01input_test", "day01input"})
+	@ValueSource(strings = { "day01input_test", "day01input" })
 	void testPart1(String filename) throws IOException {
-		Path path = Paths.get("src/test/resources/"+filename);
-
-		int currentCals = 0;
 		int maxCal = 0;
-		for(String cal : Files.lines(path).collect(Collectors.toList())) {
-			if (cal.isBlank()) {
-				if (maxCal < currentCals) {
-					maxCal = currentCals;
-				}
-				currentCals = 0;
-			} else {
-				currentCals += Integer.parseInt(cal);
-			}
-		}
-		
+		AtomicInteger group = new AtomicInteger();
+		maxCal = Files.lines(Paths.get("src/test/resources/"+filename)).collect(Collectors.groupingBy(x -> x.isBlank() ? group.getAndIncrement() : group.get())).values().stream().mapToInt(list -> list.stream().filter(s -> !s.isBlank()).mapToInt(Integer::parseInt).sum()).max().getAsInt();
+
 		if (filename.endsWith("test")) {
 			assertEquals(24000, maxCal);
 		}
-		
+
 		System.out.println(maxCal);
 	}
-	
+
 	@Test
 	void testPart2() throws Exception {
 		Path path = Paths.get("src/test/resources/day01input");
-		
+
 		List<Integer> elvesCals = new ArrayList<>();
 		int currentCals = 0;
-		for(String cal : Files.lines(path).collect(Collectors.toList())) {
+		for (String cal : Files.lines(path).collect(Collectors.toList())) {
 			if (cal.isBlank()) {
 				elvesCals.add(currentCals);
 				currentCals = 0;
