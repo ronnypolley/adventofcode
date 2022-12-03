a126773ab1f009fc5c5f4ab7e33c1b8658c24c74
Ronny Polley <ronnypolley@googlemail.com>
2022-12-03 16:35:31 +0100

    reduced lines of code by using streams

--- adventofcode-2022/src/test/java/com/adventofcode/day01/Day01Test.java
+++ adventofcode-2022/src/test/java/com/adventofcode/day01/Day01Test.java
@@ -4,15 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -21,9 +17,11 @@ class Day01Test {
 	@ParameterizedTest
 	@ValueSource(strings = { "day01input_test", "day01input" })
 	void testPart1(String filename) throws IOException {
-		int maxCal = 0;
-		AtomicInteger group = new AtomicInteger();
-		maxCal = Files.lines(Paths.get("src/test/resources/"+filename)).collect(Collectors.groupingBy(x -> x.isBlank() ? group.getAndIncrement() : group.get())).values().stream().mapToInt(list -> list.stream().filter(s -> !s.isBlank()).mapToInt(Integer::parseInt).sum()).max().getAsInt();
+		var group = new AtomicInteger();
+		var maxCal = Files.lines(Paths.get("src/test/resources/" + filename))
+				.collect(Collectors.groupingBy(x -> x.isBlank() ? group.getAndIncrement() : group.get())).values()
+				.stream().mapToInt(list -> list.stream().filter(s -> !s.isBlank()).mapToInt(Integer::parseInt).sum())
+				.max().getAsInt();
 
 		if (filename.endsWith("test")) {
 			assertEquals(24000, maxCal);
@@ -32,22 +30,19 @@ class Day01Test {
 		System.out.println(maxCal);
 	}
 
-	@Test
-	void testPart2() throws Exception {
-		Path path = Paths.get("src/test/resources/day01input");
+	@ParameterizedTest
+	@ValueSource(strings = { "day01input_test", "day01input" })
+	void testPart2(String filename) throws Exception {
+		var group = new AtomicInteger();
+		var maxCalsFromThreeElves = Files.lines(Paths.get("src/test/resources/" + filename))
+				.collect(Collectors.groupingBy(x -> x.isBlank() ? group.getAndIncrement() : group.get())).values()
+				.stream().mapToInt(list -> list.stream().filter(s -> !s.isBlank()).mapToInt(Integer::parseInt).sum())
+				.boxed().sorted(Comparator.reverseOrder()).limit(3).reduce(Integer::sum).get();
 
-		List<Integer> elvesCals = new ArrayList<>();
-		int currentCals = 0;
-		for (String cal : Files.lines(path).collect(Collectors.toList())) {
-			if (cal.isBlank()) {
-				elvesCals.add(currentCals);
-				currentCals = 0;
-			} else {
-				currentCals += Integer.parseInt(cal);
-			}
+		if (filename.endsWith("test")) {
+			assertEquals(45000, maxCalsFromThreeElves);
 		}
-		System.out.println(elvesCals);
-		System.out.println(elvesCals.stream().sorted(Comparator.reverseOrder()).limit(3).reduce(0, Integer::sum));
+		System.out.println(maxCalsFromThreeElves);
 	}
 
 }
