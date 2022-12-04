0753017757d174e269763c488c4514a7f4d51d1c
Ronny Polley <ronnypolley@googlemail.com>
2022-12-04 14:33:39 +0100

    use stackwalker to get the name of the method for output

--- adventofcode-common/src/main/java/com/adventofcode/junit/util/AdventOfCodeAssertion.java
+++ adventofcode-common/src/main/java/com/adventofcode/junit/util/AdventOfCodeAssertion.java
@@ -2,7 +2,9 @@ package com.adventofcode.junit.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.lang.StackWalker.StackFrame;
 import java.nio.file.Path;
+import java.util.Optional;
 
 public class AdventOfCodeAssertion {
 
@@ -10,7 +12,9 @@ public class AdventOfCodeAssertion {
 		if (file.toString().endsWith("test")) {
 			assertEquals(expected, actual);
 		} else {
-			System.out.println(actual);
+			Optional<StackFrame> walk = StackWalker.getInstance()
+					.walk(in -> in.filter(stack -> stack.getClassName().endsWith("Test")).findFirst());
+			System.out.println(walk.get().getClassName() + "#" + walk.get().getMethodName() + ": " + actual);
 		}
 	}
 
