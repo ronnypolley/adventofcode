9e8df69c80c0296a2641142db9a7671716ddc91a
Ronny Polley <ronnypolley@googlemail.com>
2023-12-03 22:33:38 +0100

    finally the solution of this epic riddle
    I didn't get two digit numbers correct, so it took long to find this
    out.

--- adventofcode-2022/src/test/java/com/adventofcode/day13/Day13Test.java
+++ adventofcode-2022/src/test/java/com/adventofcode/day13/Day13Test.java
@@ -5,6 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,18 +23,36 @@ class Day13Test {
 	@ParameterizedTest
 	@AdventOfCodeDailySource
 	void testPart1(Path file) throws IOException {
+		List<PacketPair> pairs = prepareInput(file);
+		AdventOfCodeAssertion.assertAdventOfCode(file, 13,
+				pairs.stream().filter(pair -> pair.list1.compareTo(pair.list2) <= 0).mapToInt(pair -> pair.id).sum());
+	}
+
+	@ParameterizedTest
+	@AdventOfCodeDailySource
+	void testPart2(Path file) throws IOException {
+		List<ListPacket> input = prepareInput(file).stream().flatMap(pair -> Stream.of(pair.list1, pair.list2))
+				.collect(Collectors.toCollection(ArrayList::new));
+		ListPacket divider1 = new ListPacket("[2]");
+		ListPacket divider2 = new ListPacket("[6]");
+
+		input.addAll(List.of(divider1, divider2));
+		input.sort(Packet::compareTo);
+
+		AdventOfCodeAssertion.assertAdventOfCode(file, 140,
+				(input.indexOf(divider1) + 1) * (input.indexOf(divider2) + 1));
+	}
+
+	private List<PacketPair> prepareInput(Path file) throws IOException {
 		List<String> readAllLines = Files.readAllLines(file);
 		List<PacketPair> pairs = new ArrayList<Day13Test.PacketPair>();
 		int id = 1;
 		for (int i = 0; i < readAllLines.size(); i += 3) {
 			var list1 = new ListPacket(readAllLines.get(i).substring(1, readAllLines.get(i).length() - 1));
 			var list2 = new ListPacket(readAllLines.get(i + 1).substring(1, readAllLines.get(i + 1).length() - 1));
-			list1.compareTo(list2);
 			pairs.add(new PacketPair(id++, list1, list2));
 		}
-		pairs.stream().filter(pair -> pair.list1.compareTo(pair.list2) < 0).forEach(pair -> logger.error(pair));
-		AdventOfCodeAssertion.assertAdventOfCode(file, 13,
-				pairs.stream().filter(pair -> pair.list1.compareTo(pair.list2) < 0).mapToInt(pair -> pair.id).sum());
+		return pairs;
 	}
 
 	private class PacketPair {
@@ -81,6 +102,33 @@ class Day13Test {
 			return String.valueOf(value);
 		}
 
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getEnclosingInstance().hashCode();
+			result = prime * result + Objects.hash(value);
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			IntegerPacket other = (IntegerPacket) obj;
+			if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+				return false;
+			return value == other.value;
+		}
+
+		private Day13Test getEnclosingInstance() {
+			return Day13Test.this;
+		}
+
 	}
 
 	private class ListPacket extends Packet {
@@ -106,9 +154,12 @@ class Day13Test {
 					list.add(new ListPacket(packet.substring(1, endIndex - 1)));
 					packet = packet.substring(endIndex);
 				} else {
-					int endIndex = packet.indexOf(",") == -1 ? 1 : packet.indexOf(",");
+					int endIndex = 1;
+					while (endIndex < packet.length() && Character.isDigit(packet.charAt(endIndex))) {
+						endIndex++;
+					}
 					list.add(new IntegerPacket(packet.substring(0, endIndex)));
-					packet = packet.length() == endIndex ? "" : packet.substring(endIndex + 1);
+					packet = packet.length() == endIndex ? "" : packet.substring(endIndex);
 				}
 			}
 		}
@@ -122,19 +173,44 @@ class Day13Test {
 			ListPacket toCompare = o instanceof IntegerPacket incomingInteger ? new ListPacket(incomingInteger)
 					: (ListPacket) o;
 
-			for (int i = 0; i < this.list.size() && i < toCompare.list.size(); i++) {
-				int compareTo = this.list.get(i).compareTo(toCompare.list.get(i));
-				if (compareTo != 0) {
-					return compareTo;
-				}
+			int compareTo = 0;
+			for (int i = 0; i < this.list.size() && i < toCompare.list.size() && compareTo == 0; i++) {
+				compareTo = this.list.get(i).compareTo(toCompare.list.get(i));
 			}
 
-			return this.list.size() - toCompare.list.size();
+			return compareTo != 0 ? compareTo : this.list.size() - toCompare.list.size();
 		}
 
 		@Override
 		public String toString() {
 			return "" + list;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + getEnclosingInstance().hashCode();
+			result = prime * result + Objects.hash(list);
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			ListPacket other = (ListPacket) obj;
+			if (!getEnclosingInstance().equals(other.getEnclosingInstance()))
+				return false;
+			return Objects.equals(list, other.list);
+		}
+
+		private Day13Test getEnclosingInstance() {
+			return Day13Test.this;
 		}
 
 	}
