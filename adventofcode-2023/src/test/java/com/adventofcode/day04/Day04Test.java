f4873e488882defb0b982e705284c1038e290fff
Ronny Polley <ronnypolley@googlemail.com>
2023-12-04 11:08:49 +0100

    the working version for part2 with recursion (really slow)

--- adventofcode-2023/src/test/java/com/adventofcode/day04/Day04Test.java
+++ adventofcode-2023/src/test/java/com/adventofcode/day04/Day04Test.java
@@ -5,8 +5,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.params.ParameterizedTest;
 
@@ -17,38 +19,68 @@ class Day04Test {
 
 	@ParameterizedTest
 	@AdventOfCodeDailySource
-	void test(Path file) throws IOException {
-//		Files.lines(file).map(s -> s.replaceAll("Card.*:", "")).map(s -> {
-//			String[] split = s.strip().split("\\|");
-//			Day04Game game = new Day04Game();
-//			game.winning = Arrays.stream(split[0].split(" ")).filter(s1 -> !s1.isBlank())
-//					.mapToInt(i -> Integer.valueOf(i.strip())).boxed().collect(Collectors.toCollection(HashSet::new));
-//			game.having = Arrays.stream(split[1].split(" ")).filter(s1 -> !s1.isBlank())
-//					.mapToInt(i -> Integer.valueOf(i.strip())).boxed().collect(Collectors.toCollection(HashSet::new));
-//			return game;
-//		}).map(g -> {
-//			g.winning.retainAll(g.having);
-//			return g.winning;
-//		}).mapToLong(w -> 1L << (w.size() - 1)).forEach(l -> System.out.print(l + " "));
-		long sum = Files.lines(file).map(s -> s.replaceAll("Card.*:", "")).map(s -> {
-			String[] split = s.strip().split("\\|");
-			Day04Game game = new Day04Game();
-			game.winning = Arrays.stream(split[0].split(" ")).filter(s1 -> !s1.isBlank())
-					.mapToInt(i -> Integer.valueOf(i.strip())).boxed().collect(Collectors.toCollection(HashSet::new));
-			game.having = Arrays.stream(split[1].split(" ")).filter(s1 -> !s1.isBlank())
-					.mapToInt(i -> Integer.valueOf(i.strip())).boxed().collect(Collectors.toCollection(HashSet::new));
-			return game;
-		}).map(g -> {
-			g.winning.retainAll(g.having);
-			return g.winning;
-		}).filter(w -> !w.isEmpty()).mapToLong(w -> 1L << (w.size() - 1)).sum();
+	void testPart1(Path file) throws IOException {
+		AdventOfCodeAssertion.assertAdventOfCode(file, 13L, mapCards(file).map(Day04Game::retaining).filter(w -> !w.isEmpty()).mapToLong(w -> 1L << w.size() - 1)
+				.sum());
+	}
 
-		AdventOfCodeAssertion.assertAdventOfCode(file, 13L, sum);
+	@ParameterizedTest
+	@AdventOfCodeDailySource
+	void testPart2(Path file) throws IOException {
+		var list = mapCards(file).toList();
+
+		var sum = 0L;
+		for (Day04Game day04Game : list) {
+			sum += computeSum(day04Game, list);
+		}
+
+		AdventOfCodeAssertion.assertAdventOfCode(file, 30L, sum);
+	}
+
+	private long computeSum(Day04Game day04Game2, List<Day04Game> list) {
+		if (day04Game2.retaining().isEmpty()) {
+			return 1L;
+		}
+
+		var sum = 1L;
+
+		for (Day04Game day04Game : list.subList(day04Game2.id + 1, day04Game2.retaining().size() + day04Game2.id + 1)) {
+			sum += computeSum(day04Game, list);
+		}
+
+		return sum;
+	}
+
+	private Stream<Day04Game> mapCards(Path file) throws IOException {
+		return Files.lines(file).map(s -> {
+			var gameSep = s.split(":");
+			var split = gameSep[1].strip().split("\\|");
+			var game = new Day04Game();
+			game.id = Integer.parseInt(gameSep[0].replaceAll("Card\\s+", "")) - 1;
+			game.winning = Arrays.stream(split[0].split(" ")).filter(s1 -> !s1.isBlank())
+					.mapToInt(i -> Integer.parseInt(i.strip())).boxed().collect(Collectors.toCollection(HashSet::new));
+			game.having = Arrays.stream(split[1].split(" ")).filter(s1 -> !s1.isBlank())
+					.mapToInt(i -> Integer.parseInt(i.strip())).boxed().collect(Collectors.toCollection(HashSet::new));
+			return game;
+		});
 	}
 
 	class Day04Game {
+		int id;
+
 		Set<Integer> winning;
 		Set<Integer> having;
+
+		Set<Integer> retaining() {
+			var set = new HashSet<>(winning);
+			set.retainAll(having);
+			return set;
+		}
+
+		@Override
+		public String toString() {
+			return id + 1 + " " + retaining() + " " + (1L << retaining().size() - 1);
+		}
 	}
 
 }
