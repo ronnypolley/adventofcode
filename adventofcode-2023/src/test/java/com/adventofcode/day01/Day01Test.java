7d0c5370e677072274c962c38f37a578003c7a3c
Ronny Polley <ronnypolley@googlemail.com>
2023-12-05 16:58:26 +0100

    added documentation for my reflections

--- adventofcode-2023/src/test/java/com/adventofcode/day01/Day01Test.java
+++ adventofcode-2023/src/test/java/com/adventofcode/day01/Day01Test.java
@@ -1,5 +1,6 @@
 package com.adventofcode.day01;
 
+import static com.adventofcode.junit.util.AdventOfCodeAssertion.assertAdventOfCode;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,7 +10,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import com.adventofcode.junit.extension.AdventOfCodeDailySource;
-import static com.adventofcode.junit.util.AdventOfCodeAssertion.assertAdventOfCode;
 
 class Day01Test {
 
@@ -18,28 +18,28 @@ class Day01Test {
 	void testPart1(Path file) throws IOException {
 		assertAdventOfCode(file, 142, combineDigits(Files.lines(file)));
 	}
-	
+
 	@ParameterizedTest
 	@AdventOfCodeDailySource
 	void testPart2(Path file) throws IOException {
 		assertAdventOfCode(file, 281, prepareForPart2(file));
 	}
 
-	private Object prepareForPart2(Path file) throws IOException {
-		// TODO Auto-generated method stub
-		return combineDigits(Files.lines(file).map(line -> line.replaceAll("one", "o1e"))
-				.map(line -> line.replaceAll("two", "t2o"))
-				.map(line -> line.replaceAll("three", "t3e"))
-				.map(line -> line.replaceAll("four", "f4r"))
-				.map(line -> line.replaceAll("five", "f5e"))
-				.map(line -> line.replaceAll("six", "s6x"))
-				.map(line -> line.replaceAll("seven", "s7n"))
-				.map(line -> line.replaceAll("eight", "e8t"))
-				.map(line -> line.replaceAll("nine", "n9e")));
+	private int prepareForPart2(Path file) throws IOException {
+		// tag::replace[]
+		return combineDigits(Files.lines(file).map(line -> line.replace("one", "o1e"))
+				.map(line -> line.replace("two", "t2o")).map(line -> line.replace("three", "t3e"))
+				.map(line -> line.replace("four", "f4r")).map(line -> line.replace("five", "f5e"))
+				.map(line -> line.replace("six", "s6x")).map(line -> line.replace("seven", "s7n"))
+				.map(line -> line.replace("eight", "e8t")).map(line -> line.replace("nine", "n9e")));
+		// end::replace[]
 	}
 
-	private Object combineDigits(Stream<String> file) throws IOException {
-		return file.map(line -> line.replaceAll("[a-z]", "")).map(line -> line.length() > 1 ? line : line + line).mapToInt(line -> Integer.parseInt(line.length() <=2 ? line : line.substring(0, 1) + line.substring(line.length()-1))).sum();
+	private int combineDigits(Stream<String> file) throws IOException {
+		return file.map(line -> line.replaceAll("[a-z]", "")).map(line -> line.length() > 1 ? line : line + line)
+				.mapToInt(line -> Integer
+						.parseInt(line.length() <= 2 ? line : line.substring(0, 1) + line.substring(line.length() - 1)))
+				.sum();
 	}
 
 }
