53b98bf312c4008d8374f6060b71f4e69e4633dd
Ronny Polley <ronnypolley@googlemail.com>
2024-12-04 21:38:57 +0100

    24/refactor packages (#67)
    * move tests to another package
    
    Signed-off-by: Ronny Polley <ronnypolley@googlemail.com>
    
    * rewrite Of to of
    
    Signed-off-by: Ronny Polley <ronnypolley@googlemail.com>
    
    * move to separate packages
    
    Signed-off-by: Ronny Polley <ronnypolley@googlemail.com>
    
    ---------
    
    Signed-off-by: Ronny Polley <ronnypolley@googlemail.com>

--- adventofcode-2024/src/test/java/com/adventofcode/day02/Day02Test.java
+++ adventofcode-2024/src/test/java/com/adventofcode/day02/Day02Test.java
@@ -1,4 +1,4 @@
-package com.adventofcode.day01;
+package com.adventofcode.day02;
 
 import com.adventofcode.junit.extension.AdventOfCodeDailySource;
 import com.adventofcode.junit.util.AdventOfCodeAssertion;
--- adventofcode-2024/src/test/java/com/adventofcode/day04/Day04Test.java
+++ adventofcode-2024/src/test/java/com/adventofcode/day04/Day04Test.java
@@ -1,4 +1,4 @@
-package com.adventofcode.day01;
+package com.adventofcode.day04;
 
 import com.adventofcode.junit.extension.AdventOfCodeDailySource;
 import com.adventofcode.junit.util.AdventOfCodeAssertion;
